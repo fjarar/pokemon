11e1387b66d56d3f2a5b08b1e311647d0617f4dd
fjarar <flavio.jara.rebolledo@gmail.com>
2022-09-08 09:53:22 -0300

    Create first component Barra

--- src/App.js
+++ src/App.js
@@ -1,24 +1,11 @@
 import React from 'react';
-import {Container, Navbar} from 'react-bootstrap';
 import 'bootstrap/dist/css/bootstrap.min.css';
+import Barra from './Barras/Barra';
 
 function App() {
   return (
     <>
-      <Navbar bg="dark" variant="dark">
-  <Container>
-    <Navbar.Brand href="#home">
-      <img
-        alt=""
-        src="/images/images.png"
-        width="30"
-        height="30"
-        className="d-inline-block align-top"
-      />{' '}
-      Pokemon
-    </Navbar.Brand>
-  </Container>
-</Navbar>
+      <Barra />
     </>
   );
 }
