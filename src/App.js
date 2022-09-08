1fe7271f0c3500d1205badc6edefec2d68f3dca3
fjarar <flavio.jara.rebolledo@gmail.com>
2022-09-08 11:10:37 -0300

    Added pokemon card and default image

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import React from 'react';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import Barra from './components/Barra/Barra';
+import PokemonCard from './components/Card/PokemonCard';
 
 function App() {
   return (
     <>
       <Barra />
+      <PokemonCard />
     </>
   );
 }
