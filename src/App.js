4bca0d9691d96e6adc82b65c16fd863cf3d90000
fjarar <flavio.jara.rebolledo@gmail.com>
2022-09-14 13:02:19 -0300

    testing how to get data from pokeapi

--- src/App.js
+++ src/App.js
@@ -1,14 +1,21 @@
-import React from 'react';
-import 'bootstrap/dist/css/bootstrap.min.css';
-import Barra from './components/Barra/Barra';
-import PokemonCard from './components/Card/PokemonCard';
-import Listado from './components/Lista/Listado';
+import React, { useEffect, useState } from "react";
+import "bootstrap/dist/css/bootstrap.min.css";
+import Barra from "./components/Barra/Barra";
+import PokemonCard from "./components/Card/PokemonCard";
+import Listado from "./components/Lista/Listado";
+import axios from "axios";
 
 function App() {
+  const [pokemon, setPokemon] = useState([]);
+  useEffect(() => {
+    axios.get("https://pokeapi.co/api/v2/pokemon").then((res) => {
+      setPokemon(res.data.results.map((p) => p.name));
+    });
+  }, [setPokemon]);
   return (
     <>
       <Barra />
-      <Listado />
+      <Listado pokemon={pokemon}/>
     </>
   );
 }
--- src/components/Lista/Listado.js
+++ src/components/Lista/Listado.js
@@ -4,10 +4,37 @@ import PokemonCard from "../Card/PokemonCard";
 import Button from "react-bootstrap/Button";
 import Card from "react-bootstrap/Card";
 
-function Listado() {
+function Listado({pokemon}) {
   const [pokemonList, setPokemonList] = useState([]);
+  const [num, setNum] = useState();
+
   useEffect(() => {
-    const endPoint = "https://pokeapi.co/api/v2/pokemon/";
+    const endPoint = `https://pokeapi.co/api/v2/pokemon/`;
+    axios
+      .get(endPoint)
+      .then((res) => {
+        const apiData = res.data;
+        setPokemonList(apiData.results);
+      })
+      .catch((err) => {
+        console.log(err);
+      });
+  }, [setPokemonList]); 
+
+  const url = "https://pokeapi.co/api/v2/pokemon/";
+  function test(name){
+    return axios.get(`${url}${name}`);
+  }
+
+  async function getPokes() {
+    const withCoords = await Promise.all(pokemonList.map(poke => test(poke.name)));
+    console.log(withCoords);
+    return withCoords;
+  }
+
+  const pokes = getPokes();
+  /* useEffect(() => {
+    const endPoint = `https://pokeapi.co/api/v2/pokemon/${pokemon}`;
     axios
       .get(endPoint)
       .then((res) => {
@@ -18,17 +45,20 @@ function Listado() {
       .catch((err) => {
         console.log(err);
       });
-  }, [setPokemonList]);
+  }, [setPokemonList]); */
+  
+
   return (
     <>
       <div className="row m-auto">
-        {pokemonList.map((pokemon, index) => {
+        {pokemon.map(p => {
           return (
-            <Card style={{ width: "18rem" }} key={index}>
+            <Card style={{ width: "18rem" }} key={p}>
               <Card.Img variant="top" src="/images/images.jfif" />
               <Card.Body>
-                <Card.Title>{pokemon.url.order}</Card.Title>
-                {console.log(pokemon.url["name"])}
+                <Card.Title>
+                  {p}
+                </Card.Title>
                 <Card.Text>
                   Some quick example text to build on the card title and make up
                   the bulk of the card's content.
